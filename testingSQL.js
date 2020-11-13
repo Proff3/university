a191773911fa9da001183ec74c839c16bfaa09be
Proff3 <vladpron2016@gmail.com>
2020-11-13 17:38:16 +0300

    test SQL

---
+++ testingSQL.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+app.listen(5000);
+
+var config = {
+    server: 'localhost',    //the IP of the machine where SQL Server runs
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'sa', // update me
+            password: 'masterkey', // update me
+        }
+    },
+    options: {
+        encrypt: false,
+        database: 'AbonentPlus',  //ssssthe username above should have granted permissions in order to access this DB.
+    }
+}
+
+var connection = new Connection(config);
+connection.on('connect', function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Connected');
+    }
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+
+
+
+
+
